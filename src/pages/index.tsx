3c67ac87726c69ee2ac9209baf2620a58a0fb93f
*TcXq99MUXk <unicast.trial.b@unicast.ne.jp>
2021-07-20 21:54:19 +0900

    🎉

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next.js Starter Template</title>
+        <meta
+          name="description"
+          content="This is Next.js Starter Template by kijikunnn"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>This is Next.js Starter Template</h1>
+      </main>
+
+      <footer></footer>
+    </div>
+  );
+}
