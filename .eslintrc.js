b0e28c2bed4b14cdac389fdccec7c719aaa55d0b
kijikunnn <unicast.trial.b@unicast.ne.jp>
2021-07-22 16:25:20 +0900

    🏗️ fix eslint & tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,4 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: "plugin:react/recommended",
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: "module",
-    project: "./tsconfig.json",
-  },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {},
-};
-module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   parserOptions: { project: "./tsconfig.json" },
