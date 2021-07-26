8e675693f1b038b79fe6722c1b0bb1954efe9e7f
kijikunnn <unicast.trial.b@unicast.ne.jp>
2021-07-27 06:44:45 +0900

    🏗️ fixed _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,9 @@
 import "styles/globals.css";
 
 import type { AppProps } from "next/app";
-import Head from "next/head";
 
 const App = (props: AppProps) => {
-  return (
-    <>
-      <Head>
-        <title>nexst</title>
-      </Head>
-      <props.Component {...props.pageProps} />
-    </>
-  );
+  return <props.Component {...props.pageProps} />;
 };
 
 export default App;
