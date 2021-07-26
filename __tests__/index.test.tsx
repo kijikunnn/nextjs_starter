202b70e2fd3858c996a542955b1b7baf62773550
kijikunnn <62680807+kijikunnn@users.noreply.github.com>
2021-07-26 16:35:08 +0900

    Update index.test.tsx

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -5,7 +5,7 @@ import "@testing-library/jest-dom/extend-expect";
 import "setimmediate";
 
 import { render, screen } from "@testing-library/react";
-import Home from "src/pages";
+import Home from "src/pages/index";
 
 it("Should render title text", () => {
   render(<Home />);
