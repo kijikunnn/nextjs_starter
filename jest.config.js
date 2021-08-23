62ecf16af21c83996e8ff0782d7dd9bbcd8cd8e2
kijikunnn <unicast.trial.b@unicast.ne.jp>
2021-08-23 11:07:26 +0900

    🔧

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  rootDir: ".",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testMatch: ["**/*.test.ts", "**/*.test.tsx"],
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   moduleNameMapper: {
-    "src/(.*)": "<rootDir>/src/$1",
+    "src/(.*)": "<rootDir>/src/",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/tests/__mocks__/fileMock.js",
     "\\.(css|less)$": "<rootDir>/tests/__mocks__/styleMock.js",
